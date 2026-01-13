052bd3f03889898d1a7714ccf82a18ee3a704746
ai-lyna <aissatlyna426@gmail.com>
2026-01-13 20:47:58 +0100

    push changes

--- backend/typeservice/models.py
+++ backend/typeservice/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 class TypeService(models.Model):
     STANDARD = 'standard'
     EXPRESS = 'express'
@@ -12,11 +11,8 @@ class TypeService(models.Model):
         (INTERNATIONAL, 'International'),
     ]
 
-    typeService = models.CharField(
-        max_length=30,
-        choices=TYPE_CHOICES,
-        unique=True
-    )
+    typeService = models.CharField(max_length=30,choices=TYPE_CHOICES,unique=True, default=STANDARD)
+    Delai = models.CharField(max_length=40 , null=True) 
 
     def __str__(self):
         return self.typeService
